a601411c3ce6673f1a4c05ed7757f1ccaf27697e
Dragon Chow <dragonpxchow@gmail.com>
2021-01-20 23:46:22 +1100

    using .env to hide mongo db user and password

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./startup/routes.js";
-//const error = require('../middleware/error');  implement it later
 
+//  loads the env variables into the process.env
 dotenv.config();
+dotenv.config({ path: ".env." + process.env.NODE_ENV }); // custome env file eg: .env.development
+
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
@@ -35,8 +37,22 @@ client.connect(err => {
 //const CONECTION_URL =
 //  "mongodb+srv://tradesman-admin:littlepiggy@reactcluster.4ttd3.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-console.log("Node server environment variables:", process.env);
+console.log("Node server environment variables:", process.env.NODE_ENV);
+console.log(
+  "REACT_APP_MONGO_DB_USER >>>>>>",
+  process.env.REACT_APP_MONGO_DB_USER
+);
+console.log(
+  "REACT_APP_MONGO_DB_PASSWORD >>>>>",
+  process.env.REACT_APP_MONGO_DB_PASSWORD
+);
 
+process.env.CONNECTION_URL =
+  "mongodb+srv://" +
+  process.env.REACT_APP_MONGO_DB_USER +
+  ":" +
+  process.env.REACT_APP_MONGO_DB_PASSWORD +
+  "@reactcluster.4ttd3.mongodb.net/project-tracker?retryWrites=true&w=majority";
 const port = process.env.PORT || "5000";
 console.log(
   "Project tracker running on port '" +
@@ -44,6 +60,7 @@ console.log(
     "' with mongo connction url:  " +
     process.env.CONNECTION_URL
 );
+
 mongoose
   .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
